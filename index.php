7cda550cfb174895af9996dc8119e23eda408621
Luka <lukadejanovic07@gmail.com>
2025-06-13 10:47:35 +0200

    start test

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Site</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welkom op mijn test site</h1>
+    <div class="vierkant"></div>
+</body>
+</html>
